1e41a166882a1847a600de5e5ca7567ee8b31efe
FranciscoEchenique <echeniquefrancisco127@gmail.com>
2023-02-13 14:37:05 -0300

    Agregar archivos de proyecto.

---
+++ ConceptosPoo/Date.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptosPoo
+{
+    public class Date
+    {
+        private int _year;
+        private int _month;
+        private int _day;
+
+        private int CheckMonth(int month)
+        {
+            if (month >= 1 && month <= 12)
+            {
+                return month;
+            }
+
+            throw new MonthException("Invalid month");
+        }
+
+        public Date(int year, int month, int day)
+        {
+            _year = year;
+            _month = CheckMonth(month);
+            _day = CheckDay(year, month, day);
+        }
+
+        private int CheckDay(int year, int month, int day)
+        {
+            if(day == 29 && month == 2 && IsLeapYear(year))
+            {
+                return day;
+            }
+
+            int[] DaysPerMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            if (day >= 1 && day <= DaysPerMonth[month])
+            {
+                return day;
+            }
+
+            throw new DayException("Invalid day");
+        }
+
+        private bool IsLeapYear(int year)
+        {
+            return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+        }
+
+        public override string ToString()
+        {
+            return $"La fecha es: {_year} / {_month} / {_day} ";
+        }
+    }
+}
---
+++ ConceptosPoo/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceptosPoo;
+
+Console.WriteLine("Conceptos POO");
+Console.WriteLine("=============");
+
+try
+{ 
+    Console.WriteLine(new Date(2022, 12, 10));
+    Console.WriteLine(new Date(2022, 12, 12));
+    Console.WriteLine(new Date(2024, 2, 29));
+    Console.WriteLine(new Date(2023, 2, 29));
+}
+catch(Exception error)
+{
+    Console.WriteLine(error.Message);
+} 
+
+
