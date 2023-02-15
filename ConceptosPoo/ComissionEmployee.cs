c8cef8d677e556b68f19433fdb91193ba072c1ce
FranciscoEchenique <echeniquefrancisco127@gmail.com>
2023-02-15 13:48:25 -0300

    Employee & SalaryEmployee

---
+++ ConceptosPoo/ComissionEmployee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptosPoo
+{
+    public class ComissionEmployee : Employee
+    {
+        public float ComissionPercentage { get; set; }
+        public decimal Sales { get; set; }
+
+        public override decimal GetValueToPay()
+        {
+            return Sales * (decimal)ComissionPercentage;
+        }
+        public override string ToString()
+        {
+            return $"{base.ToString()}" + 
+                $"\n\t Comission percentage: {ComissionPercentage:P2}" +
+                $"\n\t Sales...............: {Sales:C2}" +
+                $"\n\t Value to pay........: {GetValueToPay():C2}";
+        }
+    }
+}
--- ConceptosPoo/Program.cs
+++ ConceptosPoo/Program.cs
@@ -4,16 +4,29 @@ using ConceptosPoo;
 Console.WriteLine("Conceptos POO");
 Console.WriteLine("=============");
 
-try
-{ 
-    Console.WriteLine(new Date(2022, 12, 10));
-    Console.WriteLine(new Date(2022, 12, 12));
-    Console.WriteLine(new Date(2024, 2, 29));
-    Console.WriteLine(new Date(2023, 2, 29));
-}
-catch(Exception error)
+Employee employe1 = new SalaryEmployee()
 {
-    Console.WriteLine(error.Message);
-} 
+    Id = 1010,
+    FirstName = "Sandra",
+    LastName = "Morales",
+    BirthDate = new Date(1990, 5, 23),
+    HiringDate = new Date(2022, 1, 15),
+    IsActive = true,
+    Salary = 1815453.45M
+};
+
+Employee employe2 = new ComissionEmployee()
+{
+    Id = 2020,
+    FirstName = "Joaquín",
+    LastName = "Gonzales",
+    BirthDate = new Date(1994, 4, 22),
+    HiringDate = new Date(2022, 2, 11),
+    IsActive = true,
+    Sales = 320123123M,
+    ComissionPercentage = 0.03F
+};
+
+Console.WriteLine(employe2);
 
 
